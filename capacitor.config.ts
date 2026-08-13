485e2d4bdc8b0d0add5ba436f6f2a95376a0b160
ghhhhhhhf <irfanyunusshaikh240710@gmail.com>
2026-08-13 15:17:11 +0530

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.system.awakened',
+  appName: 'SYSTEM AWAKENED',
+  webDir: 'www'
+};
+
+export default config;
